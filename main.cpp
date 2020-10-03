9c36d0b705b21094e5d3ccb64c2b0d15eb69582d
jhd8h <jhd8h@uvawise.edu>
2020-10-03 13:56:52 -0400

    Push basic working solution.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,9 @@
+/*
+Jacob Dotson
+Tools of the Trade - Week 5
+2020-10-02
+*/
+
 #include <iostream>
 #include "wahoovian/wahoovian.h"
 
@@ -7,12 +13,18 @@
 #include <plog/Log.h> // Step1: include the headers
 #include "plog/Initializers/RollingFileInitializer.h"
 
+//main function
+//modified from example code to meet assignment requirements
+//creates two matrices, one 3x3 and one 3x4, and attempts to call
+//wahoovian on them. PLOG is used throughout as specified in assignment.
 int main(int argc, char* argv[]) {
     using namespace boost::numeric::ublas;
 
     //init logging
     plog::init(plog::debug, "wahoovian.log");
-    PLOG_INFO << "Program beginning.";
+
+    PLOG_INFO << "Program Start.";
+    PLOG_INFO << "Creating matrices.";
 
     //create matrices for use in the wahoovian function
     std::cout << "Creating example matrices" << std::endl;
@@ -26,7 +38,20 @@ int main(int argc, char* argv[]) {
         }
     }
 
+    PLOG_INFO << "Matrices created, calling wahoovian()";
+
+    //call wahoovian on the first matrix, this should succeed
+    std::cout << "First matrix before wahoovian call: " << firstMatrix << std::endl;
+    matrix<double> modifiedMatrix = wahoovian(firstMatrix);
+    std::cout << "First matrix after wahoovian call: " << modifiedMatrix << std::endl;
+
+    //call wahoovian on second matrix, this should fail
+    std::cout << "Second matrix before wahoovian call: " << secondMatrix << std::endl;
+    modifiedMatrix = wahoovian(secondMatrix);
+    std::cout << "Second matrix after wahoovian call: " << modifiedMatrix << std::endl;
+
     PLOG_INFO << "Program ending.";
 
+    //exit program
     return 0;
 }
--- wahoovian/wahoovian.cpp
+++ wahoovian/wahoovian.cpp
@@ -10,9 +10,11 @@ boost::numeric::ublas::matrix<double> wahoovian(boost::numeric::ublas::matrix<do
     int rows = input.size1();
     int columns = input.size2();
 
+    PLOG_DEBUG << "Rows are size: " << rows << " | Columns are size: " << columns; 
     //check to see if the matrix meets the function's requirements
     //if this check fails, the matrix will be returned essentially unmodified
     if (rows == columns && rows > 0 && columns > 0) {
+        PLOG_INFO << "Matrix is valid, attempting wahoovian transformation.";
         //create a matrix with reversed parameters (rows as columns and vice versa)
         boost::numeric::ublas::matrix<double> modifiedMatrix(columns, rows);
 
@@ -25,9 +27,12 @@ boost::numeric::ublas::matrix<double> wahoovian(boost::numeric::ublas::matrix<do
                 modifiedMatrix(j,i) = input(i,j) * -1;
             }
         }
+        PLOG_INFO << "Leaving function, matrix was modified";
+        return modifiedMatrix;
     }
 
     PLOG_WARNING << "Matrix is not square or has no rows or columns";
+    PLOG_INFO << "Leaving function, matrix unmodified";
     //check failed, return the original matrix
     return input;
 }
